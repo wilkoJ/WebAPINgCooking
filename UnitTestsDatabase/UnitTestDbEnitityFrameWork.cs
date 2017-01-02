ec03be17f2fb3cc0a73943cbd61f479fe9d3e1ef
WilkoJ <perruchon.wilko.isart@gmail.com>
2017-01-02 18:07:58 +0100

    testing

--- UnitTestsDatabase/UnitTestDbEnitityFrameWork.cs
+++ UnitTestsDatabase/UnitTestDbEnitityFrameWork.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using FizzWare.NBuilder;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebAPINgCooking;
 using WebAPINgCooking.Controllers;
-using System.Collections.Generic;
-using FizzWare.NBuilder;
 
 namespace UnitTestsDatabase
 {
@@ -14,15 +12,35 @@ namespace UnitTestsDatabase
         [TestMethod]
         public void TestPostInvalidValues()
         {
-            var repository = new Repository<Comment>(dbContext);
-            var users = Builder<Comment>.CreateListOfSize(5).Build();
-            foreach( var c in users )
+            var repositoryU = new Repository<User>(dbContext);
+            var repositoryR = new Repository<Recipe>(dbContext);
+            var repositoryC = new Repository<Comment>(dbContext);
+            var users = Builder<User>.CreateListOfSize(5).Build();
+            var recipes = Builder<Recipe>.CreateListOfSize(5).Build();
+            var coms = Builder<Comment>.CreateListOfSize(5).Build();
+            foreach (var c in users)
             {
-                var res = repository.Add( c );
-                Assert.AreEqual( false, res );
+                var res = repositoryU.Add(c);
+                Assert.AreEqual(true, res);
             }
-            /*foreach( var c in users )
-                repository.Delete( c.userId );*/
+            //foreach (var c in recipes)
+            //{
+            //    var res = repositoryR.Add(c);
+            //}
+            //foreach ( var c in coms )
+            //{
+            //    var res = repositoryC.Add( c );
+            //    Assert.AreEqual( false, res );
+            //}
+            foreach (var c in users)
+            {
+                var res =   repositoryU.Delete(c.userId);
+                Assert.AreEqual(true, res);
+            }
+            //foreach (var c in recipes)
+            //    repositoryR.Delete(c.userId);
+            //foreach (var c in coms)
+            //    repositoryC.Delete(c.userId);
         }
     }
 }
