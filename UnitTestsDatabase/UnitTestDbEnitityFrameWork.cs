15afe5c9cd00432dbce3a85bef3c7bb60e0f718b
WilkoJ <perruchon.wilko.isart@gmail.com>
2017-01-09 15:00:52 +0100

    unit test working

--- UnitTestsDatabase/UnitTestDbEnitityFrameWork.cs
+++ UnitTestsDatabase/UnitTestDbEnitityFrameWork.cs
@@ -22,36 +22,14 @@ namespace UnitTestsDatabase
             {
                c .userId = 0;
                 var res = repositoryU.Add(c);
-                Assert.AreEqual(false, res);
-            }
-            foreach (var c in recipes)
-            {
-                c.recipeId = 0;
-                var res = repositoryR.Add(c);
-                Assert.AreEqual(false, res);
-            }
-
-
-            foreach (var c in recipes) { 
-                var res = repositoryR.Delete(c.userId);
-                Assert.AreEqual(false, res);
+                Assert.AreNotEqual(false, res);
             }
             foreach (var c in users)
             {
                 var res = repositoryU.Delete(c.userId);
-                Assert.AreEqual(false, res);
+                Assert.AreNotEqual(false, res);
             }
-            foreach (var c in coms){
-                var res = repositoryC.Delete(c.userId);
-                Assert.AreEqual(false, res);
-            }
-            foreach (var c in coms) 
-            {
-                var res = repositoryC.Add(c);
-                Assert.AreEqual(false, res);
-            }
-            dbContext.Database.ExecuteSqlCommand("DBCC CHECKIDENT('User', RESEED, 0)");
-            dbContext.Database.ExecuteSqlCommand("DBCC CHECKIDENT('Comment', RESEED, 0)");
+            dbContext.Database.ExecuteSqlCommand("DBCC CHECKIDENT('Users', RESEED, 0)");
             dbContext.Database.ExecuteSqlCommand("DBCC CHECKIDENT('Recipe', RESEED, 0)");
         }
     }
